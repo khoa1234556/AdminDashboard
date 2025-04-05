f6b8424731279514d69ee8f7417e9612e08c3167
DKhoa04 <duykhoa12012004@gmail.com>
2025-04-05 12:47:52 +0700

    them chartjs va fix loi giao dien

--- ElectronicsSalesWebsite/wwwroot/js/chart.js
+++ ElectronicsSalesWebsite/wwwroot/js/chart.js
@@ -1,9 +1,10 @@
 ﻿
 const Utils = {
     numbers: ({ count, min, max }) => Array.from({ length: count }, () => Math.floor(Math.random() * (max - min + 1)) + min),
-    rand: (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+    random: (min, max) => Math.floor(Math.random() * (max - min + 1)) + min,
+    randomIncrement: ({ count, min, max }) => Array.from({ length: count }, (_,i) => Math.floor(Math.random() * (max - min + 1)) + min + i*1000000),
     months: ({ count }) => ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'].slice(0, count),
-    product: ({ count }) => ['Bàn trà', 'Sofa', 'Kệ tivi', 'Đèn trang trí','Bình hoa', 'Thảm sàn', 'Tủ quần áo', 'Bàn trang điểm', 'Ghế xoay','Tranh'].slice(0, count),
+    product: ({ count }) => ['Bàn gỗ', 'Ghế sofa', 'Ghế đẩu', 'Tủ quần áo', 'Tủ sách', 'Giường ngủ', 'Kệ tivi', 'Bàn ăn', 'Ghế ăn', 'Tủ giày', 'Rèm cửa', 'Bàn trà', 'Ghế bành', 'Khung ảnh', 'Đệm ngồi', 'Bàn làm việc', 'Ghế làm việc', 'Thảm trải sàn', 'Gương','Bình hoa'].slice(0, count),
     namedColor: (index) => ['#FCB454', '#00CCC0', '#E83F25', '#FF5733', '#C70039', '#4635B1', '#3A7D44', '#B771E5', '#2DAA9E', '#FFB200'][index % 9],
     color: (count) => ['#FCB454', '#00CCC0', '#E83F25', '#FF5733', '#C70039', '#4635B1', '#3A7D44', '#B771E5', '#2DAA9E', '#FFB200'].slice(0, count),
     transparentize: (color, opacity) => color + opacity,
@@ -45,6 +46,7 @@ const Utils = {
         options: {
             scales: {
                 y: {
+                    max: 90,
                     beginAtZero: true,
                     display: true,
                     
@@ -66,7 +68,7 @@ const Utils = {
                 title: {
                     display: true,
                     align: 'start',
-                    color: '#000000',
+                    //color: '#000000',
                     text: 'Số lượng buổi đấu giá theo tháng',
                     font: {
                         size: 20,
@@ -92,9 +94,10 @@ const Utils = {
             }
         },
     ]
-    document.getElementById('btn_2021').addEventListener('click', () => actions[0].handler(myChart));
-    document.getElementById('btn_2022').addEventListener('click', () => actions[0].handler(myChart));
-    document.getElementById('btn_2023').addEventListener('click', () => actions[0].handler(myChart));
+    document.getElementById('btn1_2021').addEventListener('click', () => actions[0].handler(myChart));
+    document.getElementById('btn1_2022').addEventListener('click', () => actions[0].handler(myChart));
+    document.getElementById('btn1_2023').addEventListener('click', () => actions[0].handler(myChart));
+    document.getElementById('btn1_2024').addEventListener('click', () => actions[0].handler(myChart));
     
 
 })();
@@ -104,20 +107,25 @@ const Utils = {
     const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
-            labels: Utils.product({ count: 10 }),
+            labels: Utils.product({ count: 20 }),
             datasets: [{
                 label: 'Số lượng sản phẩm tồn kho',
-                data: Utils.numbers({ count: 10, min: 10, max: 900 }),
-                backgroundColor: Utils.namedColor(Utils.rand(0, 10)),
+                data: Utils.numbers({ count: 20, min: 10, max: 900 }),
+                backgroundColor: Utils.namedColor(Utils.random(0, 10)),
                 borderRadius: 4,
             }]
         },
         options: {
             plugins: {
+                scales: {
+                    y: {
+                        max: 900,
+                    }
+                },
                 title: {
                     display: true,
                     align: 'start',
-                    color: '#000000',
+                    //color: '#000000',
                     text: 'Số lượng sản phẩm tồn kho',
                     font: {
                         size: 20,
@@ -143,8 +151,8 @@ const Utils = {
         {
             name: 'Randomize',
             handler(chart) {
-                chart.data.datasets[0].data = Utils.numbers({ count: 10, min: 10, max: 900 });
-                chart.data.datasets[0].backgroundColor = Utils.namedColor(Utils.rand(0, 10));
+                chart.data.datasets[0].data = Utils.numbers({ count: 20, min: 10, max: 900 });
+                chart.data.datasets[0].backgroundColor = Utils.namedColor(Utils.random(0, 10));
                 chart.update();
             }
         },
@@ -153,28 +161,74 @@ const Utils = {
 })();
 
 (function () {
+    function skipped(ctx, value) {
+        return ctx.p0.skip || ctx.p1.skip ? value : undefined;
+    }
+
+    function down(ctx, value) {
+        return ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+    }
     const ctx = document.getElementById('linechart');
-    const mychart = new Chart(ctx, {
+
+    const myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: Utils.getDates({ start: '2024/03/31', end: '2025/03/31' }),
             datasets: [{
-                label: 'Số lượng sản phẩm tồn kho',
-                data: Utils.numbers({ count: 365, min: 10, max: 900 }),
+                label: 'Tiền',
+                data: Utils.randomIncrement({ count: 365, min: 0, max: 150000000 }),
+                borderColor: '#06D001',
+                backgroundColor: '#4379F2',
+                segment: {
+                    borderColor: ctx => skipped(ctx, 'rgb(0,0,0,0.2)') || down(ctx, '#E83F25'),
+                    borderDash: ctx => skipped(ctx, [6, 6]),
+                },
+                
             }]
         },
         options: {
-
+            scales: {
+                y: {
+                    max: 600000000,
+                    ticks: {
+                        callback: function (value) {
+                            return value.toLocaleString('vi-VN') + ' VNĐ';
+                        }
+                    }
+                }
+            },
             responsive: true,
             plugins: {
                 legend: {
                     display: false,
                 },
-
+                tooltip: {
+                    callbacks: {
+                        label: function (context) {
+                            const label = context.dataset.label || '';
+                            const value = context.raw.toLocaleString('vi-VN'); // Format số có dấu phẩy
+                            return `${label}: ${value} VNĐ`;
+                        }
+                    }
+                }
             },
             interaction: {
                 intersect: false,
             },
         }
     });
+    const actions = [
+        {
+            name: 'Randomize',
+            handler(chart) {
+                chart.data.datasets[0].data = Utils.randomIncrement({ count: 365, min: 0, max: 150000000 }),
+                chart.update();
+            }
+        },
+    ]
+
+    document.getElementById('btn2_2021').addEventListener('click', () => actions[0].handler(myChart));
+    document.getElementById('btn2_2022').addEventListener('click', () => actions[0].handler(myChart));
+    document.getElementById('btn2_2023').addEventListener('click', () => actions[0].handler(myChart));
+    document.getElementById('btn2_2024').addEventListener('click', () => actions[0].handler(myChart));
 })();
